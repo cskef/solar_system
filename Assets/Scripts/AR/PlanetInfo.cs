f44170e1764b42b83280466dd66bfeeebe40c49d
Wilfried MEZAGO <wmezago@gmail.com>
2025-12-30 17:11:11 +0100

    Modification de PlanetInfo.cs
    Ajout de la  variable de la période de rotation

---
+++ Assets/Scripts/AR/PlanetInfo.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlanetInfo : MonoBehaviour
+{
+    public string title;
+    [TextArea] public string description;
+    public float diameterKm;
+    public float distanceToSunMillionKm;
+    public float revolutionDays;
+    public float rotationDays;
+}
